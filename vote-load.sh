bb4c71a010fc9f16f325cfc3972818246b43fcb4
quintindk <quintindk@tangentsolutions.co.za>
2020-03-27 08:09:49 +0200

    added azure docker pipeline for demo

--- vote-load.sh
+++ vote-load.sh
@@ -9,14 +9,15 @@ usage;
 exit;
 fi
 
-count=$1
-type=$2
-seconds=$3
+url=$1
+count=$2
+type=$3
+seconds=$4
 
 let i=$count-1
 while [ $i -ge 0 ];
 do
-    curl -w "$i: %{time_total} %{http_code} %{size_download} %{url_effective}\n" -o "/dev/null" --location --request POST 'http://102.133.136.50/' \
+    curl -w "$i: %{time_total} %{http_code} %{size_download} %{url_effective}\n" -o "/dev/null" --location --request POST $url \
     --header 'Content-Type: multipart/form-data; boundary=--------------------------795420350790576285548457' \
     --form 'vote='$type
     let i=i-1
